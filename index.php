859a73675699f4851c4f62ac7a89491b23bcd118
Edoardo11 <edoardomirandola2001@gmail.com>
2022-02-22 09:46:23 +0100

    updtae

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <body>
 
-    <h1>MySQLi: C R U D</h1>
+    <h1>Choose</h1>
     <br><br><br>
     <a href='login_php_db\registration.php'>User</a> <br>
     <a href='crud_mysqli\index.php'>Admin</a> <br>
