38f6501b110e917228951ac350bd645ae238c048
Edoardo11 <edoardomirandola2001@gmail.com>
2022-05-25 14:17:33 +0200

    ciO

--- login_php_db/db.php
+++ login_php_db/db.php
@@ -2,9 +2,9 @@
     $servername='localhost';
     $username='root';
     $password='';
-    $dbname = "my_db";
+    $dbname = "my_edoardomirandola01";
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
       if(!$conn){
           die('Could not Connect MySql Server:');
         }
-        
+?>       
